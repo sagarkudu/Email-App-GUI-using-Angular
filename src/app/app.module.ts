faeb2190160564e00da3d35a1fb35711a391ac61
sagarkudu <sagar978@gmail.com>
2021-01-02 16:11:57 +0530

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
+//Toolbar
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { EmailComponent } from './components/email/email.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    EmailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [MatSnackBar],
   bootstrap: [AppComponent]
